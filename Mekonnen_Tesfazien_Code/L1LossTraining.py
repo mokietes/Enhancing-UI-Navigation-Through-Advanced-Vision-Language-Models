dced6ee99ce023089d28e2922d403067b12b1e71
Mekonnen T Tesfazien <102988387+mokietes@users.noreply.github.com>
2025-07-07 23:30:04 -0700

    Update L1LossTraining.py

--- Mekonnen_Tesfazien_Code/L1LossTraining.py
+++ Mekonnen_Tesfazien_Code/L1LossTraining.py
@@ -17,3 +17,21 @@ from huggingface_hub import HfApi
 import torch.nn.functional as F
 from torch.autograd import Variable
 
+# === Environment Setup ===
+os.environ['WANDB_PROJECT'] = "Llama-3.2-11B-finetuned-SmoothL1"
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
+HF_TOKEN = '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'
+wandb.init(project=os.environ['WANDB_PROJECT'])
+
+# === Data Preprocessing Function ===
+def convert_to_conversation(sample):
+    bbox = sample.get("bbox", [0, 0, 0, 0])
+    name = sample.get("name")
+    ocr_label = sample.get("OCR")
+    resolution = sample.get("resolution")
+    description = sample.get("description")
+    language = sample.get("language")
+    platform = sample.get("platform")
+    purpose = sample.get("purpose")
+    expectation = sample.get("expectation")
+    instructions = sample.get("instruction")
