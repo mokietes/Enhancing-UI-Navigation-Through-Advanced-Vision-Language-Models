fc508132a7be1eb5ab5990b76c59778b0687168d
Mekonnen T Tesfazien <102988387+mokietes@users.noreply.github.com>
2025-07-23 22:26:58 -0700

    Update mainL2.py

--- Mekonnen_Tesfazien_Code/mainL2.py
+++ Mekonnen_Tesfazien_Code/mainL2.py
@@ -16,3 +16,22 @@ from transformers import (
 import torch.nn as nn
 import torch.nn.functional as F
 
+# === Environment Setup ===
+os.environ['WANDB_PROJECT'] = "Llama-3.2-11B-finetuned-waveUI-l2"
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
+HF_TOKEN = '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'
+wandb.init(project=os.environ['WANDB_PROJECT'])
+
+# === Data Preprocessing ===
+def convert_to_conversation(sample):
+    bbox = sample.get("bbox", [0, 0, 0, 0])
+    name = sample.get("name")
+    ocr_label = sample.get("OCR")
+    resolution = sample.get("resolution")
+    description = sample.get("description")
+    language = sample.get("language")
+    platform = sample.get("platform")
+    purpose = sample.get("purpose")
+    expectation = sample.get("expectation")
+    instructions = sample.get("instruction")
+
