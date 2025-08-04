aac6df1c3ea72e01e05e8197db555fd5f2152439
Mekonnen T Tesfazien <102988387+mokietes@users.noreply.github.com>
2025-08-03 22:07:51 -0700

    Update mainUpgrade.py

--- Mekonnen_Tesfazien_Code/mainUpgrade.py
+++ Mekonnen_Tesfazien_Code/mainUpgrade.py
@@ -188,3 +188,33 @@ class WandBLoggingCallback(TrainerCallback):
         train_loss = kwargs.get("train_loss", 0)
         wandb.log({"train_loss": train_loss})
 
+# === Training Arguments ===
+training_args = SFTConfig(
+    per_device_train_batch_size=2,
+    gradient_accumulation_steps=2,
+    warmup_steps=500,
+    num_train_epochs=1,
+    learning_rate=2e-5,
+    fp16=not is_bf16_supported(),
+    bf16=is_bf16_supported(),
+    logging_steps=500,
+    optim="adamw_8bit",
+    weight_decay=0.01,
+    lr_scheduler_type="linear",
+    seed=3407,
+    output_dir="./outputs/check",
+    report_to="wandb",
+    remove_unused_columns=False,
+    dataset_text_field="",
+    dataset_kwargs={"skip_prepare_dataset": True},
+    dataset_num_proc=4,
+    max_seq_length=2048,
+    fp16_full_eval=True,
+    per_device_eval_batch_size=4,
+    eval_accumulation_steps=4,
+    eval_strategy="steps",
+    eval_steps=1000,
+    save_steps=500,
+    save_total_limit=5,
+    resume_from_checkpoint="./outputs/check",
+)
